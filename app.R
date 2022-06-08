c15d0b1fe15a0af5038ebab99bc829c63ca0b514
vagdur <vilhelm.agdur@gmail.com>
2022-06-08 20:14:28 +0200

    Initialise the barebones of the app, set up RStudio project

---
+++ app.R
@@ -0,0 +1,17 @@
+library(shiny)
+
+# Define UI for our application - it should essentially be a better version of a Jupyter notebook for
+# presentation.
+ui <- fluidPage(
+    # Title of the page
+    titlePanel("SVM, Kernels, and Non-linear Data", windowTitle = "WASP AIML Project"),
+    "A bunch of content will go here!"
+)
+
+# Server logic specifying how the UI should work:
+server <- function(input, output) {
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
